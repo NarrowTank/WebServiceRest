a337e7091e6e7c720fe1033f010e09c802cebf23
NarrowTank <104443383+NarrowTank@users.noreply.github.com>
2023-03-18 19:10:11 -0300

    Delete labrestLuis_Gabriel/labrestisaJos/labrest directory

--- labrestLuis_Gabriel/labrestisaJos/labrest/src/main/java/engcom/std/labrest/exceptions/PessoaNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package engcom.std.labrest.exceptions;
-
-public class PessoaNaoEncontradaException extends RuntimeException{
-    public PessoaNaoEncontradaException(long id) {
-         super("Não foi possível encontrar pessoa com o id: " + id);
-         }
-}
